43395678ac88df17fad82aecb9f14fde9ce93bb9
Tony Jian <tonytonyjan@gmail.com>
2014-06-16 03:53:50 +0800

    use content_raw instread of content

--- lib/dodo.rb
+++ lib/dodo.rb
@@ -3,7 +3,8 @@ api = Tjplurk::API.new
 api.real_time do |plurk|
   api.request '/APP/Alerts/addAllAsFriends'
   next unless plurk['type'] == 'new_plurk'
-  content = plurk['content'].gsub(/<[^>]*>/, '')
+  # content = plurk['content'].gsub(/<[^>]*>/, '')
+  content = plurk['content_raw']
 
   if response = Robot.instance.respond(content, plurk_uid: plurk['user_id'].to_s)
     response_params = {
