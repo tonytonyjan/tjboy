2d51293cd76df140b266976cbf34bcd3fc073506
Tony Jian <tonytonyjan@gmail.com>
2014-06-15 19:25:16 +0800

    refactoring

--- lib/dodo.rb
+++ lib/dodo.rb
@@ -1,36 +1,16 @@
-require 'tjplurk/robot'
-
 api = Tjplurk::API.new
-updated_at = Topic.maximum(:updated_at)
-robot = Tjplurk::Robot.new Topic.where(subscriber: nil)
 
 api.real_time do |plurk|
-  jj plurk
   api.request '/APP/Alerts/addAllAsFriends'
   next unless plurk['type'] == 'new_plurk'
-
   content = plurk['content'].gsub(/<[^>]*>/, '')
 
-  maximum_updated_at = Topic.maximum(:updated_at)
-  private_robot = nil
-  if user = User.find_by(plurk_uid: plurk['user_id'].to_s)
-    if topics = user.subscribed_topics.presence
-      private_robot = Tjplurk::Robot.new topics
-    end
-  end
-
-  if updated_at < maximum_updated_at
-    robot = Tjplurk::Robot.new Topic.where(subscriber: nil)
-    updated_at = maximum_updated_at
-  end
-  order = [robot, private_robot].shuffle!
-  if response = order.pop.respond(content).presence || order.pop.respond(content).presence
+  if response = Robot.instance.respond(content, plurk_uid: plurk['user_id'].to_s)
     response_params = {
       plurk_id: plurk['plurk_id'],
       content: response,
       qualifier: ':'
     }
-
     api.request '/APP/Responses/responseAdd', response_params
   end
 end
