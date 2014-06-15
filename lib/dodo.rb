7373b1c1cca5bc00ad4bf574f39a2de00ac80c05
Tony Jian <tonytonyjan@gmail.com>
2014-06-15 16:50:27 +0800

    private robot

--- lib/dodo.rb
+++ lib/dodo.rb
@@ -2,21 +2,29 @@ require 'tjplurk/robot'
 
 api = Tjplurk::API.new
 updated_at = Topic.maximum(:updated_at)
-robot = Tjplurk::Robot.new Topic.all
+robot = Tjplurk::Robot.new Topic.where(subscriber: nil)
 
 api.real_time do |plurk|
+  jj plurk
   api.request '/APP/Alerts/addAllAsFriends'
   next unless plurk['type'] == 'new_plurk'
 
   content = plurk['content'].gsub(/<[^>]*>/, '')
 
-  maximum_updated_at = Topic.maximum(:updated_at)  
-  if updated_at < maximum_updated_at
-    robot = Tjplurk::Robot.new Topic.all
-    updated_at = maximum_updated_at
+  maximum_updated_at = Topic.maximum(:updated_at)
+  private_robot = nil
+  if user = User.find_by(plurk_uid: plurk['user_id'].to_s)
+    if topics = user.subscribed_topics.presence
+      private_robot = Tjplurk::Robot.new topics
+    end
   end
 
-  if response = robot.respond(content).presence
+  if updated_at < maximum_updated_at
+    robot = Tjplurk::Robot.new Topic.where(subscriber: nil)
+    updated_at = maximum_updated_at
+  end
+  order = [robot, private_robot].shuffle!
+  if response = order.pop.respond(content).presence || order.pop.respond(content).presence
     response_params = {
       plurk_id: plurk['plurk_id'],
       content: response,
